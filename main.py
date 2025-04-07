a6862fd00322c20adb38db1256d6b5d50e25f410
darcilas1 <danielarcila48@gmail.com>
2025-04-07 11:12:43 -0500

    Fix - Menu Button

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os
 
 def login(driver, username, password):
     driver.get("https://visiong.iagree.co/iAgree/faces/login.xhtml")
+    driver.maximize_window()
     assert "i Agree 2.0" in driver.title
     user = driver.find_element(By.NAME, "loginForm:j_idt22")
     user.send_keys(username)
@@ -17,7 +18,6 @@ def login(driver, username, password):
 
     # Get the captcha text element
     captcha_text = driver.find_element(By.ID, "captcha") 
-    print(captcha_text.text)  # Print the captcha text to the console
     captcha_input = driver.find_element(By.NAME, "loginForm:j_idt26")
     captcha_input.send_keys(captcha_text.text)
     captcha_input.send_keys(Keys.RETURN)
@@ -36,27 +36,17 @@ def select_campaign(driver):
     time.sleep(1)  # Wait for the page to load
 
     # Click on the "Gestionar" link
-    menu_button = driver.find_element(By.XPATH, "//*[@id='menu-button']")
-    menu_closed = driver.find_elements(By.CLASS_NAME, "menu-button-rotate")
-
-    if menu_closed:
-        print("Menu not open, clicking to open")
-        menu_button.click()
-        time.sleep(1)
-        manage_link = driver.find_element(By.XPATH, "//*[@id='mainForm:mnGestionar']/a")
-        manage_link.click()
-    else:
-        print("Menu already open")
-        time.sleep(1)
-        manage_link = driver.find_element(By.XPATH, "//*[@id='mainForm:mnGestionar']/a")
-        manage_link.click()
+    manage_link = driver.find_element(By.XPATH, "//*[@id='mainForm:mnGestionar']/a")
+    manage_link.click()
 
 if __name__ == "__main__":
     # Load environment variables from .env file
     load_dotenv()
 
     # Set up the driver
-    driver = webdriver.Chrome()
+    # driver = webdriver.Chrome()
+    # driver.get("https://visiong.iagree.co/iAgree/faces/login.xhtml")
+    # driver.maximize_window()
 
     # Get the username and password from environment variables
     username = os.environ.get("USERNAME_VG")
