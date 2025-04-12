3e9f7b689aed7e20132e0c8daf014a2b9cd10a85
darcilas1 <danielarcila48@gmail.com>
2025-04-12 09:50:57 -0500

    Added the payment_agreement function

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 import time
 import os
+
 
 def login(driver, username, password):
     driver.get("https://visiong.iagree.co/iAgree/faces/login.xhtml")
@@ -25,32 +28,38 @@ def login(driver, username, password):
 def select_campaign(driver):
     time.sleep(3)  # Wait for the page to load
 
-    # Click on the "Grupo 3" link
-    grupo_3_link = driver.find_element(By.XPATH, "//td[contains(text(), '3 | LEONISA CASTIGO')]")
-    grupo_3_link.click()
+    # # Click on the "Grupo 3" link
+    # grupo_3_link = driver.find_element(By.XPATH, "//td[contains(text(), '3 | LEONISA CASTIGO')]")
+    # grupo_3_link.click()
 
+    # time.sleep(1)  # Wait for the page to load
+    # grupo_3_link = driver.find_element(By.XPATH, "//*[@id='mainForm:dtCampanas:0:j_idt204']/span[1]")
+    # grupo_3_link.click()
+
+    # time.sleep(1)  # Wait for the page to load
+
+    # # Click on the "Gestionar" link
+    # manage_link = driver.find_element(By.XPATH, "//*[@id='mainForm:mnGestionar']/a")
+    # manage_link.click()
+
+    # click on the "LEONISA MASTER"
+    ingresar_leonisa_link= driver.find_element(By.XPATH, "//*[@id='mainForm:j_idt106:0:j_idt112']/span")
+    ingresar_leonisa_link.click()
     time.sleep(1)  # Wait for the page to load
-    grupo_3_link = driver.find_element(By.XPATH, "//*[@id='mainForm:dtCampanas:0:j_idt204']/span[1]")
-    grupo_3_link.click()
-
-    time.sleep(1)  # Wait for the page to load
-
-    # Click on the "Gestionar" link
-    manage_link = driver.find_element(By.XPATH, "//*[@id='mainForm:mnGestionar']/a")
-    manage_link.click()
 
 def client_management(driver, cedula):
     time.sleep(3)
 
-    # Click on the "Buscar" link
+    # Search for the client by ID
     cedula_input = driver.find_element(By.XPATH, "//*[@id='topBarForm:itBuscarGenerico']")
     cedula_input.send_keys(cedula)
+    # click on the search button
     driver.find_element(By.XPATH, "//*[@id='topBarForm:j_idt56_button']/span[1]").click()
-    time.sleep(2)
+    time.sleep(1)
 
     # Select client
     driver.find_element(By.XPATH, "//*[@id='mainForm:idDtDeudoresObligaciones_data']/tr/td[3]").click()
-    time.sleep(2)
+    time.sleep(1)
 
     # Select phone number
     driver.find_element(By.XPATH, "//*[@id='mainForm:pgRenderisarDatosContacto:dtTelefonosDeudorData_data']/tr[1]/td[2]").click()
@@ -59,23 +68,55 @@ def client_management(driver, cedula):
     # Select "Contacto titular" 
     driver.find_element(By.XPATH, "//*[@id='mainForm:dtAgreeGuiones_data']/tr[11]/td/span").click()
     time.sleep(2)
+
     # Select keep contact
-    driver.find_element(By.XPATH, "//*[@id='mainForm:j_idt12760']").click()
-    time.sleep(1)
+    wait = WebDriverWait(driver, 10)  # Wait until 10 secs max
+    keep_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(),'MANTENER')]")))
+    keep_button.click()
+    time.sleep(2)
 
     # Select "Compromiso de pago"
     driver.find_element(By.XPATH, "//*[@id='mainForm:dtAgreeGuiones_data']/tr[7]/td/span").click()
     time.sleep(2)
     # Select skip 
-    driver.find_element(By.XPATH, "//*[@id='mainForm:j_idt12768']/span").click()
+    driver.find_element(By.XPATH, "//span[contains(text(),'OMITIR')]").click()
     time.sleep(1)
+
+def payment_agreement(driver, agreement_value):
+    time.sleep(2)
 
     #Select the payment agreement
     driver.find_element(By.XPATH, "//*[@id='mainForm:pgPanelDatosDeContacto:dtObligTogg2_data']/tr/td[2]/div/div[2]/span").click()
     time.sleep(2)
 
-    # select "Acuerdo"
+    # Select "Acuerdo"
     driver.find_element(By.XPATH, "//*[@id='mainForm:pgPanelDatosDeContacto:nuevoAcuerdo']/span[2]").click()
+    time.sleep(2)
+
+    # Select "omitir y agregar nuevo"
+    wait = WebDriverWait(driver, 15)  # Wait until 10 secs max
+    skip_add_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='mainForm:j_idt12859']/span")))
+    skip_add_button.click()
+    time.sleep(2)
+
+    # add the agreement value
+    agreement_value_field = driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:j_idt12884_input']")
+    agreement_value_field.send_keys(agreement_value)
+    time.sleep(1)
+
+    # Select the confirmation hour
+    driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:j_idt12892']/div[3]/span").click()
+    driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:j_idt12892_15']").click()
+    time.sleep(1)
+
+    # Select "Efecto Acuerdo"
+    driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:j_idt12910']/div[3]/span").click()
+    driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:j_idt12910_1']").click()
+    time.sleep(1)
+
+    # Select "posibilidad de cumplimiento"
+    driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:j_idt12923']/tbody/tr/td[2]/div/div[2]").click()
+
 
 
 if __name__ == "__main__":
@@ -90,14 +131,15 @@ if __name__ == "__main__":
     # Get the username and password from environment variables
     username = os.environ.get("USERNAME_VG")
     password = os.environ.get("PASSWORD_VG")
-    cedula= os.environ.get("CEDULA_PRUEBA")
+    cedula = os.environ.get("CEDULA_PRUEBA")
+    agreement_value = os.environ.get("VALOR_ACUERDO")
 
     options = Options()
     options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(options=options)
 
-    print(username, password)
 
     login(driver, username, password)
     select_campaign(driver)
     client_management(driver, cedula)
+    payment_agreement(driver, agreement_value)
