487a78a55fb6d73b3df9c58ecd314ee7fc02028d
DanielArcila <danielarcila@grupovg.com.co>
2025-05-06 16:59:49 -0500

    Update .gitignore and enhance main.py for improved error handling and performance

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options 
 from selenium.webdriver.support.ui import WebDriverWait # Used for the wait
 from selenium.webdriver.support import expected_conditions as EC # Used for the wait
+from selenium.webdriver.common.action_chains import ActionChains
 from dotenv import load_dotenv
 from selenium.common.exceptions import TimeoutException # Used for the try except block
 import time
 import pandas as pd 
 import os
+import traceback
 
 
 def login(driver, username, password):
-    driver.get("https://visiong.iagree.co/iAgree/faces/login.xhtml")
+    driver.get(os.environ.get("URL_VG"))
     driver.maximize_window()
-    time.sleep(2)  # Wait for the page to load
+    time.sleep(4)  # Wait for the page to load
     assert "i Agree 2.0" in driver.title
     user = driver.find_element(By.NAME, "loginForm:j_idt22")
     user.send_keys(username)
@@ -29,7 +31,7 @@ def login(driver, username, password):
     captcha_input.send_keys(Keys.RETURN)
 
 def select_campaign(driver):
-    time.sleep(3)  # Wait for the page to load
+    time.sleep(5)  # Wait for the page to load
 
     # # Click on the "Grupo 3" link
     # grupo_3_link = driver.find_element(By.XPATH, "//td[contains(text(), '3 | LEONISA CASTIGO')]")
@@ -48,45 +50,73 @@ def select_campaign(driver):
     # click on the "LEONISA MASTER"
     ingresar_leonisa_link= driver.find_element(By.XPATH, "//*[@id='mainForm:j_idt106:0:j_idt112']/span")
     ingresar_leonisa_link.click()
-    time.sleep(1)  # Wait for the page to load
+    time.sleep(3)  # Wait for the page to load
 
 def client_management(driver, cedula):
-    time.sleep(3)
+    time.sleep(5)
 
     # Search for the client by ID
     cedula_input = driver.find_element(By.XPATH, "//*[@id='topBarForm:itBuscarGenerico']")
+    acciones = ActionChains(driver)
+    #double click on the input field
+    acciones.double_click(cedula_input).perform()
+    time.sleep(2)
+    #send keys of the cedula
     cedula_input.send_keys(cedula)
+    time.sleep(3)
     # click on the search button
-    driver.find_element(By.XPATH, "//*[@id='topBarForm:j_idt56_button']/span[1]").click()
-    time.sleep(1)
+    cedula_input.send_keys(Keys.ENTER)
+    time.sleep(3)
 
     # Select client
-    driver.find_element(By.XPATH, "//*[@id='mainForm:idDtDeudoresObligaciones_data']/tr/td[3]").click()
-    time.sleep(1)
+    try:
+        # When is the first search
+        driver.find_element(By.XPATH, "//*[@id='mainForm:idDtDeudoresObligaciones_data']/tr/td[3]").click()
+        time.sleep(3)
+    except Exception as e:
+        # When is the second search
+        driver.find_element(By.XPATH, "//*[@id='mainForm:dtDeudoresBusqueda_data']/tr/td[5]").click()
+        time.sleep(3)
+        # click on "Gestionar cliente"
+        driver.find_element(By.XPATH, "//*[@id='mainForm:j_idt2498']/span").click()
+        time.sleep(4)
 
     # Select phone number
     driver.find_element(By.XPATH, "//*[@id='mainForm:pgRenderisarDatosContacto:dtTelefonosDeudorData_data']/tr[1]/td[2]").click()
-    time.sleep(2)
+    time.sleep(4)
 
     # Select "Contacto titular" 
     driver.find_element(By.XPATH, "//*[@id='mainForm:dtAgreeGuiones_data']/tr[11]/td/span").click()
-    time.sleep(2)
+    time.sleep(4)   
 
-    # Select keep contact
-    wait = WebDriverWait(driver, 10)  # Wait until 10 secs max
-    keep_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(),'MANTENER')]")))
-    keep_button.click()
-    time.sleep(2)
+    try:
+        # Select keep contact
+        wait = WebDriverWait(driver, 5)  # Wait until 5 secs max
+        keep_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(),'MANTENER')]")))
+        keep_button.click()
+        time.sleep(4)
+    except TimeoutException:
+        wait = WebDriverWait(driver, 4)  # Wait until 2 secs max
+        causales_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='mainForm:j_idt1748_data']/tr/td")))
+        causales_button.click()
+        manejo_codigo_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='mainForm:j_idt1752_data']/tr[6]/td/span")))
+        manejo_codigo_button.click()
+        time.sleep(3)
+        driver.find_element(By.XPATH, "//*[@id='mainForm:j_idt1758']/span[2]").click()
+        time.sleep(4)
+        print("causales button clicked")
+        print("TimeoutException: Element not found")
+
 
     # Select "Compromiso de pago"
     driver.find_element(By.XPATH, "//*[@id='mainForm:dtAgreeGuiones_data']/tr[7]/td/span").click()
-    time.sleep(2)
+    time.sleep(4)
     # Select skip 
     driver.find_element(By.XPATH, "//span[contains(text(),'OMITIR')]").click()
-    time.sleep(1)
+    time.sleep(3)
 
 def payment_agreement(driver, agreement_value,agreement_comment, agreement_date):
-    time.sleep(2)
+    time.sleep(4)
 
     # add the agreement comment
     agreement_comment_field = driver.find_element(By.XPATH, "//*[@id='mainForm:idObsAgreeScript']")
@@ -94,18 +124,18 @@ def payment_agreement(driver, agreement_value,agreement_comment, agreement_date)
 
     #Select the payment agreement
     driver.find_element(By.XPATH, "//*[@id='mainForm:pgPanelDatosDeContacto:dtObligTogg2_data']/tr/td[2]/div/div[2]/span").click()
-    time.sleep(2)
+    time.sleep(4)
 
     # Select "Acuerdo"
     driver.find_element(By.XPATH, "//*[@id='mainForm:pgPanelDatosDeContacto:nuevoAcuerdo']/span[2]").click()
-    time.sleep(2)
+    time.sleep(4)
 
     # Select "omitir y agregar nuevo"
     try:
-        wait = WebDriverWait(driver, 3)  # Wait until 2 secs max
+        wait = WebDriverWait(driver, 5)  # Wait until 2 secs max
         skip_add_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='mainForm:j_idt12859']/span")))
         skip_add_button.click()
-        time.sleep(2)
+        time.sleep(4)
         print("Skip and add button clicked")
     except TimeoutException:
         print("TimeoutException: Element not found")
@@ -113,74 +143,91 @@ def payment_agreement(driver, agreement_value,agreement_comment, agreement_date)
     # add the agreement value
     agreement_value_field = driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:j_idt12884_input']")
     agreement_value_field.send_keys(agreement_value)
-    time.sleep(1)
+    time.sleep(3)
 
     # Selecet the date
     agreement_date_field = driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:j_idt12888_input']")
     agreement_date_field.send_keys(agreement_date)
-    time.sleep(1)
+    time.sleep(3)
     agreement_date_field.send_keys(Keys.ENTER)
     driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:ingresarAcuerdo']/div[2]").click()
-    time.sleep(2)
+    time.sleep(4)
 
     # Select the confirmation hour
     driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:j_idt12892']/div[3]/span").click()
     driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:j_idt12892_15']").click()
-    time.sleep(2)
+    time.sleep(4)
 
     # Select "Efecto Acuerdo"
     driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:j_idt12910']/div[3]/span").click()
-    time.sleep(0.2)
+    time.sleep(2)
     driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:j_idt12910_2']").click()
-    time.sleep(1)
+    time.sleep(3)
 
     # Select "posibilidad de cumplimiento"
     driver.find_element(By.XPATH, "//*[@id='mainForm:idAccordionPanelAcuerdos:j_idt12923']/tbody/tr/td[2]/div/div[2]").click()
+    time.sleep(2)
+    # # Esperar a que el usuario presione Enter
+    # input("Presiona Enter para cerrar el navegador y terminar el script...")
+    # driver.quit()  # Cierra el navegador solo después de que el usuario presione Enter
 
+    #select "adicionar acuerdo"
+    driver.find_element(By.XPATH, "//*[@id='mainForm:j_idt13113']/span[2]").click()
+    time.sleep(4)
+
+    # Select "guardar"
+    try:
+        # Click on first button "Guardar"
+        driver.find_element(By.XPATH, "//*[@id='mainForm:j_idt13106']").click()
+        time.sleep(4)
+    except Exception as e:
+        # Click on second button "Guardar"
+        driver.find_element(By.XPATH, "//*[@id='mainForm:j_idt13127']/span[2]").click()
+        time.sleep(4)
 
 if __name__ == "__main__":
     # Load environment variables from .env file
     load_dotenv()
 
-    # Set up the driver
-    # driver = webdriver.Chrome()
-    # driver.get("https://visiong.iagree.co/iAgree/faces/login.xhtml")
-    # driver.maximize_window()
-
     # Get the username and password from environment variables
     username = os.environ.get("USERNAME_VG")
     password = os.environ.get("PASSWORD_VG")
     agreement_comment = ("cobranza digital (gestión IA)")
-
-
+    
     options = Options()
     options.add_experimental_option("detach", True)
+
+    driver = webdriver.Chrome(options=options)
+    login(driver, username, password)
+    select_campaign(driver)
 
     csv_path = r"C:\Users\57318\compromiso pruebas RPA leonisa.csv"
     df = pd.read_csv(csv_path, sep=";")
 
+    resultados = []
+
     for index, row in df.iterrows():
         cedula = str(row["NIT"]).strip()
-        phone_number = str(row["TELEFONO"]).strip()
         agreement_value = str(row["VALOR"]).strip()
         agreement_date = str(row["FECHA"]).strip()
-        efecto_acuerdo = str(row["EFECTO ACUERDO"]).strip()  
 
         try:
-            # Set up the driver for each iteration
-            driver = webdriver.Chrome(options=options)
-            login(driver, username, password)
-            select_campaign(driver)
             client_management(driver, cedula)
             payment_agreement(driver, agreement_value, agreement_comment, agreement_date)
 
-            print(f"✅ Client {cedula} processed correctly.\n")
-
+            resultados.append({"NIT": cedula,"estado": "Procesado"})
             
 
         except Exception as e:
-            print(f"❌ error processing client {cedula}: {e}")
-            try:
-                driver.quit()
-            except:
-                pass
+            resultados.append({"NIT": cedula,"estado": "Error"})
+            # Mostrar el traceback completo en la terminal
+            traceback.print_exc()
+            
+
+
+    # Guardar resultados
+    resultado_df = pd.DataFrame(resultados)
+    resultado_df.to_csv("resultado_gestion.csv", index=False, encoding="utf-8-sig")
+
+    driver.quit()
+    print("Prceso terminado correctamente")
